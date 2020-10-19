2ae6e4fda9705261f11ecf9ab6c931801e20fc7e
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-19 11:45:30 +0530

    problem

--- problem1.sh
+++ problem1.sh
@@ -4,10 +4,15 @@ dice1=$((RANDOM%7 +1))
 dice2=$((RANDOM%7 +1))
 sum=0
 count=0
-for  (( i=6; $i!=$dice1 || $i!=$dice2; ))
+while(1)
 do
+if [ $dice1==6 -a $dice2==6 ]
+then
+break
+else
 count=$((count+1))
 sum=$((sum+dice1+dice2))
+fi
 done
 echo $sum
 echo $count
